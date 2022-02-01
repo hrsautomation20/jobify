42b9d7784c140648f02a5812aec5e36f1a74f4b4
Hardik Rola <hrsautomation20@gmail.com>
2022-02-01 12:13:37 -0500

    Updated Error Handling, Hashing Password, JWT, Controller inital Setup

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
 import express from 'express';
-const app = express()
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('Welcome')
-})
+import dotenv from 'dotenv';
+dotenv.config();
+import 'express-async-errors'
 
-const PORT = process.env.PORT || 5000
+//DB and Auth user
+import connectDB from './db/connect.js';
 
-app.listen(PORT,()=>{
-    console.log(`Server is listening on port ${PORT}!!`)
-})
+//routers
+import authRouter from './routes/authRoutes.js';
+import jobRouter from './routes/jobsRoutes.js';
+
+//middleware
+import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from './middleware/error-handler.js';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}!!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
