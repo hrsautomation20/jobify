4e30386c4f86fcd4b3a965a1a2f7e75c82a3103d
Hardik Rola <hrsautomation20@gmail.com>
2022-02-03 11:13:03 -0500

    Login user func front end

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
   const navigate = useNavigate();
   const [values, setValues] = useState(initialState);
 
-  const { user, isLoading, showAlert, displayAlert, registerUser } =
+  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
     useAppContext();
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -35,7 +35,8 @@ const Register = () => {
     }
     const currentUser = { name, email, password };
     if (isMember) {
-      console.log('already a member');
+      // console.log('already a member');
+      loginUser(currentUser);
     } else {
       registerUser(currentUser);
     }
